7aed950b47d15f256037113fc8ac4eb95d617b86
FaithKwamboka <okongofaith3@gmail.com>
2022-04-26 22:58:27 +0300

    Adding more content

--- Faith_Okongo/app/views.py
+++ Faith_Okongo/app/views.py
@@ -1,14 +1,22 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/services')
 def services():
     ls=["Massage", "Full body Scrub", "Waxing","Sauna","Pedicure","Manicure","Hair Dressing"]
     return render_template('services.html',names=ls)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 
